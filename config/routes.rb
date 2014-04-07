ebb74694befd06c88f67714172931c0638687168
Lynne Cooney <ilynne@gmail.com>
2014-04-06 20:32:30 -0700

    Shop resource added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MyrAccounting::Application.routes.draw do
+  resources :shops
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
