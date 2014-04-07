061a5974841c15b2b6a7749bb77433d4f0cd5710
Lynne Cooney <ilynne@gmail.com>
2014-04-06 20:25:09 -0700

    bare rails accounting app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyrAccounting::Application.config.secret_key_base = 'af78efbfa6b6ca1a25653f69e4782f48a222e2ca1522cababea9201012b8090d428eedc2675c03114c3d7db6fe8f2a3b8dd4742c1954e5f7b6debe897bfa5322'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyrAccounting::Application.config.session_store :cookie_store, key: '_myr-accounting_session'
