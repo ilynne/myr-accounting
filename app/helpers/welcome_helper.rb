0174f49d910f05ee6c504ef041adda02e8a5aff6
Lynne Cooney <ilynne@gmail.com>
2014-04-17 16:39:49 -0700

    Week 2 homework Lynne Cooney

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,8 @@
 module WelcomeHelper
+  def we_are_excited (options={})
+    if options[:name].present?
+      "We are so excited to see you, #{options[:name]}!"
+    end
+  end
 end
+# app/helpers/my_custom_helper.rb
