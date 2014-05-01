7cae1ba881f37ad06980287da3c6fc4e18924aba
ilynne <ilynne@gmail.com>
2014-05-01 16:20:00 -0700

    added relationships and named scope

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+
+	belongs_to :shop
+	has_many :products, through: :shops
+
+	scope :recent, order("created_at desc").limit(10)
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,6 @@
 class Shop < ActiveRecord::Base
+
+	has_many :products
+	has_many :orders
+	
 end
