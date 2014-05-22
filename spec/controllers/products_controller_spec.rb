25c921657931353bbf3c4b7e8d1bca7bd9d988da
ilynne <ilynne@gmail.com>
2014-05-22 16:05:45 -0700

    products page authentication tests work

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -8,7 +8,7 @@ describe ProductsController do
     describe "GET#index" do
       it "does not allow access" do
         get "index"
-        puts response.body.inspect
+        # puts response.body.inspect
         expect(response.status).to be 302
       end
     end
@@ -16,15 +16,16 @@ describe ProductsController do
 
   describe "authenticated user" do
     before(:each) do
-      visit new_user_session
-      fill_in "Email", :with => admin.email
-      fill_in "Password", :with => admin.password
-      click_button "Log in"
+      visit new_user_session_path
+      fill_in "Email", :with => user.email
+      fill_in "Password", :with => user.password
+      click_button "Sign in"
     end
 
-    describe "GET#index" do
+    describe "products page" do
       it "gets the index view" do
-        get "index"
+        visit products_path
+        # puts response.body.inspect
         expect(response.status).to be 200
       end
     end
